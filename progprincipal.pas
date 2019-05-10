23abf718186a954230c3c1c6da3741c840be5eeb
MAKYOON <k.li@ludus-academie.com>
2019-05-10 12:44:20 +0200

    Modification progprincipal pour remplacer le vecteur u par l'objet balle de type GameObject

--- progprincipal.pas
+++ progprincipal.pas
@@ -4,48 +4,49 @@ program Game;
 uses ball,Vect,GameObj,crt;
 
 var 
+	pong : balle;
 	scalaire: integer;
-	u,v,w: Vect2D;
+	v,w: Vect2D;
 
 BEGIN
 	clrscr;
 	//TEST DE l'UNIT VECT2D
 
 	//TEST DES ASSESSEURS ET MUTATEURS GET/SET
-	u.posx := 4; //soit u un vecteur (4)...
-	u.posy := 2;// ...               (2)
+	pong.obj.vecteur.posx := 4; //soit pong.obj.vecteur un vecteur (4)...
+	pong.obj.vecteur.posy := 2;// ...               (2)
 	v.posx := 5;//soit v un vecteur (5)...
 	v.posy := 7;// ...              (7)
-	w.posx := getVectPosX(u); //on test la fonction getVectPosX, résultat attendu ici : 4
-	w.posy := getVectPosY(u); //on test la fonction getVectPosY, résultat attendu ici : 2
-	writeln('Position X du vecteur u : ', w.posx);
-	writeln('Position Y du vecteur u : ', w.posy);
-	setVectPosX(u,3);//on test la procédure setVectPosX en mettant u.posx à 3
-	setVectPosY(u,1);//on test la procédure setVectPosY en mettant u.posy à 1
-	writeln('Position X du vecteur u apres changement : ', u.posx); //on vérifie que le changement à eu lieu. résultat attendu : 3
-	writeln('Position Y du vecteur u apres changement : ', u.posy);//on vérifie que le changement à eu lieu. résultat attendu : 1
+	w.posx := getVectPosX(pong.obj.vecteur); //on test la fonction getVectPosX
+	w.posy := getVectPosY(pong.obj.vecteur); //on test la fonction getVectPosY
+	writeln('Position X du vecteur pong.obj.vecteur : ', w.posx);
+	writeln('Position Y du vecteur pong.obj.vecteur : ', w.posy);
+	setVectPosX(pong.obj.vecteur,3);//on test la procédure setVectPosX en mettant pong.obj.vecteur.posx à 3
+	setVectPosY(pong.obj.vecteur,1);//on test la procédure setVectPosY en mettant pong.obj.vecteur.posy à 1
+	writeln('Position X du vecteur pong.obj.vecteur apres changement : ', pong.obj.vecteur.posx); //on vérifie que le changement à eu lieu.
+	writeln('Position Y du vecteur pong.obj.vecteur apres changement : ', pong.obj.vecteur.posy);//on vérifie que le changement à eu lieu. 
 
 	//MAINTENANT TEST DES OPERATIONS USUELLES SUR LES VECTEURS
 
 	//ADDITION VECTORIELLE
-	w := additionVect(u,v); //test de la fonction additionVect. u(3,1) et v(5,7)
-    writeln('Soit w = u+v, on a w.posx : ', w.posx , ' et w.posy ' , w.posy);
+	w := additionVect(pong.obj.vecteur,v); //test de la fonction additionVect. pong.obj.vecteur(3,1) et v(5,7)
+    writeln('Soit w = pong.obj.vecteur+v, on a w.posx : ', w.posx , ' et w.posy ' , w.posy);
 
 	//SOUSTRACTION VECTORIELLE
-	w := soustractionVect(u,v); //test de la fonction soustractionVect. u(3,1) et v(5,7)
-    writeln('Soit w = u-v, on a w.posx : ', w.posx , ' et w.posy ' , w.posy);	
+	w := soustractionVect(pong.obj.vecteur,v); //test de la fonction soustractionVect. pong.obj.vecteur(3,1) et v(5,7)
+    writeln('Soit w = pong.obj.vecteur-v, on a w.posx : ', w.posx , ' et w.posy ' , w.posy);	
 
     //MULTIPLICATION D'UN VECTEUR PAR UN SCALAIRE
-    v := mult1VectScal(3,u);//test de la fonction mult1VectScal. u(3,1)
-    writeln('Soit v = 3u, on a v.posx : ', v.posx , ' et v.posy ' , v.posy);
+    v := mult1VectScal(3,pong.obj.vecteur);//test de la fonction mult1VectScal. pong.obj.vecteur(3,1)
+    writeln('Soit v = 3pong.obj.vecteur, on a v.posx : ', v.posx , ' et v.posy ' , v.posy);
 
     //PRODUIT SCALAIRE
-    scalaire := prodScalaire(u,v);//test de la fonction prodScalaire. u : (3,1) et v : (9,3)
-    writeln('Soit u(x1,y1) et v(x2,y2) on a u.v = x1y1 + x2y2, donc u.v = ', scalaire);
+    scalaire := prodScalaire(pong.obj.vecteur,v);//test de la fonction prodScalaire. pong.obj.vecteur : (3,1) et v : (9,3)
+    writeln('Soit pong.obj.vecteur(x1,y1) et v(x2,y2) on a pong.obj.vecteur.v = x1y1 + x2y2, donc pong.obj.vecteur.v = ', scalaire);
 
     //PRODUIT VECTORIEL
-    scalaire := prodVectoriel(u,v);//test de la fonction prodVectoriel. u : (3,1) et v : (9,3)
-    writeln('Soit u(x1,y1) et v(x2,y2) on a u*v = x1y2 - x2y1, donc u*v =  ', scalaire);
+    scalaire := prodVectoriel(pong.obj.vecteur,v);//test de la fonction prodVectoriel. pong.obj.vecteur : (3,1) et v : (9,3)
+    writeln('Soit pong.obj.vecteur(x1,y1) et v(x2,y2) on a pong.obj.vecteur*v = x1y2 - x2y1, donc pong.obj.vecteur*v =  ', scalaire);
 
 readln;
 END.
